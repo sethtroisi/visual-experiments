d5172321c20fba60b9d76040c8c1a5b69dade657
Seth Troisi <sethtroisi@google.com>
2022-02-10 01:41:56 -0800

    Day 1: Fades with Fred's Imagemagick scripts

--- day1/crossfade.sh
+++ day1/crossfade.sh
@@ -10,7 +10,7 @@ then
     mv "Flying over Florida _ Relaxing 4K Ocean Videos _ Slowmo Overhead Waves [fG_X-cx5Szg].webm" "$VID_1"
 
     # Splice out 20 good seconds
-    ffmpeg -ss 00:00:01 -i "$VID_1" -t 00:00:20 -vf scale=1920:1080 -an vid1_1080_20s.mp4
+    ffmpeg -ss 00:00:01 -i "$VID_1" -t 00:00:20 -an vid1_20s.mp4
 fi
 
 VID_2="Hummingbirds.webm"
@@ -20,48 +20,55 @@ then
     mv "See Hummingbirds Fly, Shake, Drink in Amazing Slow Motion _ National Geographic [RtUQ_pz5wlo].webm" "Hummingbirds.webm"
 
     # Splice out 20 good seconds
-    ffmpeg -ss 00:00:24.2 -i "$VID_2" -t 00:00:20 -an vid2_1080_20s.mp4
+    ffmpeg -ss 00:00:24.2 -i "$VID_2" -t 00:00:20 -an vid2_20s.mp4
 fi
 
-
-if false
+FPS=25
+if [ ! -d "frames_wave"] || [ ! -d "frames_hummingbird" ]
 then
     echo "Splitting frames out from 20s videos"
     mkdir -p "frames_wave" "frames_hummingbird"
-    time ffmpeg -i "vid1_1080_20s.mp4" -filter:v fps=30 "frames_wave/%0d.jpg"
-    time ffmpeg -i "vid2_1080_20s.mp4" -filter:v fps=30 "frames_hummingbird/%0d.jpg"
+    # -q:v quality video (jpg 2-31, 31 is worst)
+    time ffmpeg -hide_banner -i "vid1_20s.mp4" -filter:v "scale=1920x1080,fps=$FPS" -q:v 4  "frames_wave/%0d.jpg"
+    time ffmpeg -hide_banner -i "vid2_20s.mp4" -filter:v "scale=1920x1080,fps=$FPS" -q:v 4  "frames_hummingbird/%0d.jpg"
 fi
 
-TOTAL_FRAMES=600
+
+# TODO min of frames_hummingbird/ and frames_wave/
+TOTAL_FRAMES=$(ls frames_hummingbird/ | wc -l)
+echo "Found $TOTAL_FRAMES frames"
 
 function threshold() {
     echo "Thresholding $1 with Fred's wonderful scripts"
     # http://www.fmwconcepts.com/imagemagick/ptilethresh/index.php
-    mkdir -p "threshold"
+    mkdir -p "threshold_$1"
 
     for f in `seq 1 $TOTAL_FRAMES`; do
         percent=$(bc <<< "scale=2; $f * 100 / $TOTAL_FRAMES")
-        ptilethresh -p $percent $1/$f.jpg threshold/$f.jpg
-
+        # output gif per http://www.fmwconcepts.com/imagemagick/ptilethresh/index.php
+        ptilethresh -p $percent $1/$f.jpg "threshold_$1"/$f.gif
     done
 }
 
 function merge() {
-    echo "Merging $1 * threshold over $2"
+    echo "Merging $1 over $2"
 
     mkdir -p "merged"
     rm -f merged/*.jpg
+    set +x
     for f in `seq 1 $TOTAL_FRAMES`; do
         # Things to try
         #   - Only randomness (every X frames)
         #   - Blend randomness + constant ptile
-        #   - lighten / darken
+        #   - Compose {Lighten, Darken}
+        #   - perlin -a {0.5, 1, 2, $percent}
 
         # Add some randomness every X frames
-        if [ $(($f % 10)) -eq 1 ]
+        if [ $(($f % $FPS)) -eq 1 ]
         then
-            perlin 192x108 noise.png > /dev/null
-            convert -resize 1920x1080 noise.png noise.png
+            echo "New noise @ $f"
+            # T
+            perlin 1920x1080 -a 0.5 noise.png > /dev/null
         fi
 
         # See
@@ -69,28 +76,33 @@ function merge() {
         # https://legacy.imagemagick.org/Usage/layers/#evaluate-sequence
         # https://legacy.imagemagick.org/Usage/transform/#evaluate
 
-        # I gave up on doing this as one step because it seems impossible
         # Blend noises
-        convert threshold/$f.jpg -compose Lighten noise.png -composite temp.png
+        #convert threshold/$f.jpg -compose Lighten noise.png -composite temp.png
         # Under, Over, Mask => Output
-        convert $2/$f.jpg $1/$f.jpg temp.png -composite merged/$f.jpg
-    done
+        #convert $2/$f.jpg $1/$f.jpg temp.png -composite merged/$f.jpg
 
-    rm -f "noise.png" "temp.png"
+        # (Under * Black_of_mask) + (Over * White_of_mask) => Output
+        convert "$1/$f.jpg" "$2/$f.jpg" \
+            \( "threshold_$2/$f.gif" "noise.png" -compose Darken -composite \) \
+            -composite "merged/$f.jpg"
+    done
+    set -x
 }
 
 
-if true
+if false
 then
     set +x
     time threshold frames_hummingbird
-    time merge frames_hummingbird frames_wave
+    time threshold frames_wave
     set -x
-    ffmpeg -hide_banner -stats -loglevel warning  -y -framerate 30 -start_number 1 -i merged/%d.jpg -c:v libx264 bird_over_waves.mp4
+fi
 
-    set +x
-    threshold frames_hummingbird
-    merge frames_hummingbird frames_wave
-    set -x
-    ffmpeg -hide_banner -stats -loglevel warning  -y -framerate 30 -start_number 1 -i merged/%d.jpg -c:v libx264 waves_over_bird.mp4
+if true
+then
+    time merge frames_hummingbird frames_wave
+    ffmpeg -hide_banner -stats -loglevel warning  -y -framerate $FPS -start_number 1 -i merged/%d.jpg -c:v libx264 bird_over_waves.mp4
+
+    time merge frames_wave frames_hummingbird
+    ffmpeg -hide_banner -stats -loglevel warning  -y -framerate $FPS -start_number 1 -i merged/%d.jpg -c:v libx264 waves_over_bird.mp4
 fi
